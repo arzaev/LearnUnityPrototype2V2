49fde3463e01ad5bb6ee5727a9724882a4479b19
Ilya <arzaevilya@gmail.com>
2021-06-23 02:44:22 +0200

    done with prototypev2

--- Challenge 2/Scripts/PlayerControllerX.cs
+++ Challenge 2/Scripts/PlayerControllerX.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
 
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private GameObject currentDogPrefab;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (dogPrefab != null
+            && currentDogPrefab == null
+            && Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            currentDogPrefab = Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
         }
     }
 }
--- Challenge 2/Scripts/SpawnManagerX.cs
+++ Challenge 2/Scripts/SpawnManagerX.cs
@@ -16,7 +16,8 @@ public class SpawnManagerX : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        int randInterval = Random.Range(3, 5);
+        InvokeRepeating("SpawnRandomBall", startDelay, randInterval);
     }
 
     // Spawn random ball at random x position at top of play area
@@ -26,7 +27,8 @@ public class SpawnManagerX : MonoBehaviour
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        int rand = Random.Range(0, 2);
+        Instantiate(ballPrefabs[rand], spawnPos, ballPrefabs[rand].transform.rotation);
     }
 
 }
